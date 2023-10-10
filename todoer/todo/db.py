71ec0d3af620c0b2ec44e5003537014ea7d5dbae
Taslehoof <seracon@gmail.com>
2023-10-10 14:42:19 -0300

    DB Script: Creating the scritp to initialize the DB

--- todoer/todo/db.py
+++ todoer/todo/db.py
@@ -11,15 +11,30 @@ def get_db():
                 user = current_app.config['DATABASE_USER'],
                 password = current_app.config['DATABASE_PASSWORD'],
                 database = current_app.config['DATABASE']
-                )
+        )
         g.c = g.db.cursor(dictionary = True)
     return g.db, g.c
 
-def close_db(e-None):
+def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
 
+def init_db():
+    db, c= get_db()
+
+    for i in instructions:
+        c.execute(i)
+
+    db.commit()
+
+@click.command('init_db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Base de datos inicializada')
+
 def init_app(app):
     app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
