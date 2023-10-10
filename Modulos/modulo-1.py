bad1d8af0f5d6ef344921d305e8ffac745cbd520
Taslehoof <seracon@gmail.com>
2023-10-10 01:47:32 -0300

    refactoring branch

--- Modulos/modulo-1.py
+++ Modulos/modulo-1.py
@@ -9,16 +9,4 @@ c = CamelCase()
 s = 'esta oracion necesita CamelCase!'
 
 camelcase = c.hump(s)
-
-#import modulos as xs
-from modulos import saludo,mascotas
-from camelcase import CamelCase
-
-print(mascotas)
-saludo('Sergio')
-
-c = CamelCase()
-s = 'esta oracion necesita CamelCase!'
-
-camelcase = c.hump(s)
 print(camelcase)
