64696f5f3f349bef43ab15e9831ba886b3cb4370
Taslehoof <seracon@gmail.com>
2023-10-11 18:28:34 -0300

    Fix: Bugs resolve and implementing bew stuff

--- todoer/todo/db.py
+++ todoer/todo/db.py
@@ -16,17 +16,17 @@ def get_db():
         g.c = g.db.cursor(dictionary = True)
     return g.db, g.c
 
-def close_dh(e-None):
+def close_dh(e=None):
     db = g.pop('db', None)
      
-     if db is not None:
+    if db is not None:
          db.close()
 
 
 def init_app(app):
     app.teardown_appcontext(close_db)
 
-        g.c = g.db.cursor(dictionary = True)
+    g.c = g.db.cursor(dictionary = True)
     return g.db, g.c
 
 def close_db(e=None):
