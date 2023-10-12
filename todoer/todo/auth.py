30defe083e1781f33956b5048ca5948e01e40dbd
Taslehoof <seracon@gmail.com>
2023-10-12 14:32:50 -0300

    Complete procedure to Auth

--- todoer/todo/auth.py
+++ todoer/todo/auth.py
@@ -4,7 +4,7 @@ from flask import (
     Blueprint, flash, g, render_template, request, url_for, sesions
          )
 
-from werkzeug.secuity import check_password_hash, generatepassword_hash
+from werkzeug.secuity import check_password_hash, generate_password_hash
 
 from todo.db import get_db
 
@@ -20,3 +20,46 @@ def rgister():
         c.execute(
             "select id from user mhere username = %d"
         )
+        if not username:
+            error = 'Username es requerido'
+        if not password:
+            error = 'Password es requerido'
+        elif c.fetchone() is not None:
+            error = 'Usuario {} se encuentra registrado.'.format(username)
+        
+
+        if error is None:
+             c.execute(
+                "insert into user (username, password) values (%s,%s)",
+                (username, generate_password_hash(password))
+             )
+             db.commit()
+         
+             return redirect(url_for('auth.login'))
+
+        flash(error)
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db, c = get_db()
+        error = None
+        c.execute(
+                'select * from user where username = %s', (username)
+                 )
+        user = c.fetchone()
+
+        if user is None:
+            error = 'Usuario y/o contraseña invalida'
+        elif not check_password_hash(user['password'], password):
+            error = 'Usuario y/O contraseña invalida'
+
+        if error is None:
+            session.clear()
+                session['user_id'] = user['id']
+                return redirect(url_for('index'))
+        flash(error)
+    return render_template('auth/login.html')
