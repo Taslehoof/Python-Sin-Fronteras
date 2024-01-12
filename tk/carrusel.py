a4a848bc06b7618c2a8a9f98b35f9b0ebdb00539
Taslehoof <seracon@gmail.com>
2024-01-11 22:18:18 -0300

    Changes and updates

--- tk/carrusel.py
+++ tk/carrusel.py
@@ -11,12 +11,43 @@ img4 = ImageTk.PhotoImage(Image.open('imagenes/4.jpg'))
 
 lista = [img1, img2, img3, img4]
 
-def adelante(img_num):
-    pass
 
 l = Label(root, image = img1)
 l.grid(row=0, column=0, columnspan=3)
 
+def adelante(img_num):
+   global l
+   global btn_atras
+   global btn_adelante
+
+   l.grid_forget()
+   l = Label(root, image=lista[img_num])
+   btn_atras = Button(root, text='<--', command=lambda: atras(img_num -1))
+   btn_adelante = Button(root, text='-->', command=lambda: adelante(img_num +1))
+
+   if img_num == 3:
+       btn_adelante = Button(root, text='N/A', state=DISABLED)
+
+   l.grid(row=0,column=0, columnspan=3)
+   btn_atras.grid(row=1, column=0)
+   btn_adelante.grid(row=1, column=2)
+
+def atras(img_num):
+   global l
+   global btn_atras
+   global btn_adelante
+
+   l.grid_forget()
+   l = Label(root, image=lista[img_num])
+   btn_atras = Button(root, text='<--', command=lambda: atras(img_num -1))
+   btn_adelante = Button(root, text='-->', command=lambda: adelante(img_num +1))
+
+   if img_num == 0:
+       btn_atras = Button(root, text='N/A', state=DISABLED)
+
+   l.grid(row=0,column=0, columnspan=3)
+   btn_atras.grid(row=1, column=0)
+   btn_adelante.grid(row=1, column=2)
 
 btn_atras = Button(root, text='N/A', state=DISABLED)
 btn_adelante = Button(root, text='-->', command= lambda: adelante(1))
