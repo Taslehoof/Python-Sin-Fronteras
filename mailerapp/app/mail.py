21cc6a334744a06900c97b126be01827a037a081
Taslehoof <seracon@gmail.com>
2024-01-27 01:39:23 -0300

    Update and upgrade
    Update repo and upgrade of funcionality and code.

--- mailerapp/app/mail.py
+++ mailerapp/app/mail.py
@@ -1,10 +1,41 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request, flash
 )
+
+from app.db import get_db
 
 bp = Blueprint('mail', __name__, url_prefix="/")
 
 
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template('mails/index.html')
+    db, c = get_db()
+
+    c.execute("SEELCT * FROM email")
+    mails = c.fetchall()
+
+    return render_template('mails/index.html', mails=mails)
+
+
+@bp.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        content = request.form.get('content')
+        errors = []
+
+        if not email:
+            errors.append('Email es obligatorio')
+        if not subject:
+            errors.append('Asunto es obligatorio')
+        if not content:
+            errors.append('Contenido es obligatorio')
+
+        if len(errors) == 0:
+            pass
+        else:
+            for error in errors:
+                flash(error)
+
+    return render_template('mails/create.html')
